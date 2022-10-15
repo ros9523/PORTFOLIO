d038482c7a7a4a89a329590e8749dbe4c2843bbd
ros9523 <rofidia95@gmail.com>
2022-10-14 23:51:47 -0600

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import image from "../images/image.png";
+import image from "../images/Image.png";
 import "./styles/nav.css";
 
 const Nav = () => {
